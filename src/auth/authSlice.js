7ba001df9bc11286e34c00c65c9c3fb95fb973f5
Sharan-ram <sharanram1993@gmail.com>
2020-03-30 01:09:14 +0530

    Implement user registration

--- src/auth/authSlice.js
+++ src/auth/authSlice.js
@@ -1,11 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isAuthenticated: false,
   jwt: null,
   user: null,
   isLoading: false,
-  error: {}
+  error: null
 };
 
 const authSlice = createSlice({
@@ -13,40 +14,84 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     loading(state, _) {
-      state.isLoading = true;
+      return Object.assign({}, state, {
+        isLoading: true
+      });
     },
     register(state, action) {
-      const { payload, error } = action;
+      const {
+        payload: { error, errorData, jwt, user }
+      } = action;
       if (error) {
-        state.error = payload;
-        state.isLoading = false;
+        return Object.assign({}, state, {
+          error: errorData,
+          isLoading: false,
+          isAuthenticated: false
+        });
       }
-      const { jwt, user } = payload;
-      state = {
+      return Object.assign({}, state, {
         isAuthenticated: true,
         jwt,
         user,
         isLoading: false,
         error: {}
-      };
+      });
     },
     login(state, action) {
-      const { payload, error } = action;
+      const {
+        payload: { error, errorData, jwt, user }
+      } = action;
       if (error) {
-        state.error = payload;
-        state.isLoading = false;
+        return Object.assign({}, state, {
+          error: errorData,
+          isLoading: false,
+          isAuthenticated: false
+        });
       }
-      const { jwt, user } = payload;
-      state = {
+      return Object.assign({}, state, {
         isAuthenticated: true,
         jwt,
         user,
         isLoading: false,
         error: {}
-      };
+      });
     }
   }
 });
 
 export const { loading, register, login } = authSlice.actions;
+
+export const registerAsync = payload => async dispatch => {
+  dispatch(loading());
+  try {
+    const res = await axios({
+      url: `${process.env.REACT_APP_API_URL}/api/users`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: payload
+    });
+    dispatch(register(res.data));
+  } catch (err) {
+    const {
+      response: { data, status }
+    } = err;
+    console.error(err);
+    let payload = {};
+    if (typeof data === String) {
+      payload = {
+        msg: data,
+        status
+      };
+    } else {
+      payload = {
+        ...data.errors[0],
+        status
+      };
+    }
+    dispatch(register({ error: true, errorData: payload }));
+  }
+};
+
 export default authSlice.reducer;
