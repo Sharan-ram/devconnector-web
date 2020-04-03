c8d13c7bf32dce63a839665b3593626f68cca849
Sharan-ram <sharanram1993@gmail.com>
2020-04-03 17:08:23 +0530

    Load jwt from localStorage

--- src/store.js
+++ src/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./auth/authSlice";
+import authSlice, { login as loginAction } from "./auth/authSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice
   }
 });
+
+const jwt = localStorage.getItem("jwt");
+if (jwt !== null) {
+  store.dispatch(loginAction({ jwt }));
+}
 
 export default store;
