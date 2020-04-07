415e47cbc6d3af5c9836ecf10b225d564c647b89
Sharan-ram <sharanram1993@gmail.com>
2020-04-07 20:51:27 +0530

    Write a common api function for all API calls, convert getProfile to use the new function

---
+++ src/api/index.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const api = async options => {
+  const jwt = localStorage.getItem("jwt");
+  const {
+    loadingAction,
+    dataAction,
+    method = "GET",
+    url,
+    payload,
+    access = "private",
+    stateSlice,
+    dispatch
+  } = options;
+  dispatch(loadingAction());
+
+  let config = {};
+
+  let headers = {};
+  if (access === "private") {
+    headers["x-auth-token"] = jwt;
+  }
+
+  if (method === "PUT" || method === "POST") {
+    headers["Content-Type"] = "application/json";
+    config.data = payload;
+  }
+
+  if (Object.keys(headers).length) {
+    config.headers = headers;
+  }
+
+  try {
+    const res = await axios({
+      ...config,
+      method,
+      url
+    });
+    dispatch(dataAction({ [stateSlice]: res.data }));
+  } catch (err) {
+    console.error(err);
+    const {
+      response: { data, status }
+    } = err;
+    let payload = {};
+    if (typeof data === String) {
+      payload = {
+        msg: data,
+        status
+      };
+    } else {
+      payload = {
+        ...data.errors[0],
+        status
+      };
+    }
+    dispatch(dataAction({ error: true, errorData: payload }));
+  }
+};
+
+export default api;
