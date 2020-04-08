1dbfe08065041b1769fa20f951046b17da39c2c1
Sharan-ram <sharanram1993@gmail.com>
2020-04-08 17:22:19 +0530

    Refrain from sending jwt for API request from the component

--- src/hooks/useMyProfile.js
+++ src/hooks/useMyProfile.js
@@ -3,13 +3,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { getMyProfileAsync } from "../features/Profile/profileSlice";
 
 const useMyProfile = () => {
-  const jwt = useSelector(state => state.auth.jwt);
   const { isLoading, myProfile, error } = useSelector(state => state.profile);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getMyProfileAsync({ jwt }));
-  }, [jwt, dispatch]);
+    dispatch(getMyProfileAsync());
+  }, [dispatch]);
 
   return [isLoading, myProfile, error];
 };
