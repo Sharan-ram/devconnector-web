d4dd24f36cc9a4c7d2adde55ea414819df5454dd
Sharan-ram <sharanram1993@gmail.com>
2020-04-09 17:12:32 +0530

    Implement Add experience feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
 import Layout from "./components/layout";
 
 function App() {
-  return <Layout />;
+  return (
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <Layout />
+    </MuiPickersUtilsProvider>
+  );
 }
 
 export default App;
