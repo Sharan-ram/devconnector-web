eb75c84d3bacdb4b3b9570c80d602a1d9d299b40
Sharan-ram <sharanram1993@gmail.com>
2020-04-11 13:32:23 +0530

    Implement edit and delete education

--- src/features/Dashboard.js
+++ src/features/Dashboard.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -18,26 +13,13 @@ import Paper from "@material-ui/core/Paper";
 import useMyProfile from "../hooks/useMyProfile";
 import moment from "moment";
 
-const EducationForm = () => {
-  return (
-    <Dialog open={true}>
-      <DialogTitle>
-        <DialogTitle>Add Experience</DialogTitle>
-        <DialogContent>Details here</DialogContent>
-      </DialogTitle>
-      <DialogActions>Buttons will come here</DialogActions>
-    </Dialog>
-  );
-};
-
 const Dashboard = () => {
   const [isLoading, profile] = useMyProfile();
   const [redirectToEditProfile, toggleEditProfile] = useState(false);
   const [redirectToAddExperience, toggleAddExperience] = useState(false);
   const [experienceId, setEditExperience] = useState();
   const [redirectToAddEducation, toggleAddEducation] = useState(false);
-
-  const dispatch = useDispatch();
+  const [educationId, setEditEducation] = useState();
 
   if (isLoading) return <div>Loading Profile...</div>;
 
@@ -54,18 +36,27 @@ const Dashboard = () => {
   if (redirectToAddEducation)
     return <Redirect to="/profile/me/add-education" />;
 
+  if (educationId)
+    return <Redirect to={`/profile/me/edit-education/${educationId}`} />;
+
   const { experience, education, user } = profile;
 
-  const sortedExperience = experience
-    .map((exp) => {
-      const { from, to } = exp;
-      return {
-        ...exp,
-        from: moment(from),
-        to: to && moment(to),
-      };
-    })
-    .sort((a, b) => b.from.diff(a.from));
+  const sortByFromDate = (arr) => {
+    return arr
+      .map((obj) => {
+        const { from, to } = obj;
+        return {
+          ...obj,
+          from: moment(from),
+          to: to && moment(to),
+        };
+      })
+      .sort((a, b) => b.from.diff(a.from));
+  };
+
+  const sortedExperience = sortByFromDate(experience);
+
+  const sortedEducation = sortByFromDate(education);
 
   return (
     <div>
@@ -104,6 +95,43 @@ const Dashboard = () => {
                     </TableCell>
                     <TableCell>
                       <Button onClick={() => setEditExperience(_id)}>
+                        Edit
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div>
+        <h4>Education Credentials</h4>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>School</TableCell>
+                <TableCell>Degree</TableCell>
+                <TableCell>From</TableCell>
+                <TableCell>To</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sortedEducation.map((ed) => {
+                const { _id, school, degree, from, to, current } = ed;
+                const toDate = to && to.format("DD-MM-YYYY");
+                return (
+                  <TableRow key={_id}>
+                    <TableCell>{school}</TableCell>
+                    <TableCell>{degree}</TableCell>
+                    <TableCell>{from.format("DD-MM-YYYY")}</TableCell>
+                    <TableCell>
+                      {current === true ? "Present" : toDate}
+                    </TableCell>
+                    <TableCell>
+                      <Button onClick={() => setEditEducation(_id)}>
                         Edit
                       </Button>
                     </TableCell>
