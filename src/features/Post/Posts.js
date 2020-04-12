0e84cfccf727b961160777d207981a2db79652fe
Sharan-ram <sharanram1993@gmail.com>
2020-04-12 14:07:23 +0530

    Render all posts

--- src/features/Post/Posts.js
+++ src/features/Post/Posts.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
 
 import Button from "@material-ui/core/Button";
+import Like from "@material-ui/icons/ThumbUp";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { getAllPostsAsync, createPostAsync } from "./postSlice";
 
+const useStyle = makeStyles({
+  post: {
+    display: "grid",
+    gridTemplateColumns: "1fr 4fr",
+  },
+});
+
 const Posts = () => {
+  const classes = useStyle();
   const [post, setPost] = useState("");
+  const { isLoading, posts } = useSelector((state) => state.post);
 
   const dispatch = useDispatch();
 
@@ -18,6 +31,8 @@ const Posts = () => {
   const createPost = () => {
     dispatch(createPostAsync({ text: post }));
   };
+
+  if (isLoading) return <div>Loading profile...</div>;
 
   return (
     <div>
@@ -33,6 +48,41 @@ const Posts = () => {
         <Button disabled={post === ""} onClick={createPost}>
           Submit
         </Button>
+      </div>
+      <div>
+        {posts !== null ? (
+          posts.map((post) => {
+            const { likes, id, name, text, date, comments } = post;
+            return (
+              <div key={id} className={classes.post}>
+                <div>{name}</div>
+                <div>
+                  <div>
+                    <Typography component="p">{text}</Typography>
+                  </div>
+                  <div>
+                    <Typography component="span">
+                      Posted on {moment(date).format("DD-MM-YYYY")}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Button>
+                      <Like />
+                      {likes.length === 0 ? "" : likes.length}
+                    </Button>
+                    <Button>
+                      Discussion
+                      {comments.length === 0 ? "" : comments.length}
+                    </Button>
+                    <Button>Delete Post</Button>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <div>No posts</div>
+        )}
       </div>
     </div>
   );
