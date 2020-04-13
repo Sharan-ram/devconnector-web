3ab2fe2508d231c6f1dababd2bd9bee584d0e4c7
Sharan-ram <sharanram1993@gmail.com>
2020-04-13 13:03:02 +0530

    Render all profiles

--- src/components/layout/PublicRoutes.js
+++ src/components/layout/PublicRoutes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import { UserProfiles } from "../../features/Profile";
+import { UserProfiles, SingleProfile } from "../../features/Profile";
 
 const PublicRoutes = ({ match: { path } }) => {
   return (
     <Switch>
       <Route exact path={`${path}/profiles/all`} component={UserProfiles} />
+      <Route exact path={`${path}/profiles/:id`} component={SingleProfile} />
     </Switch>
   );
 };
