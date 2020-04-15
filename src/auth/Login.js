0c8542d81d6e6c92ea1203ae27b25b2d3734b1cf
Sharan-ram <sharanram1993@gmail.com>
2020-04-15 19:53:35 +0530

    Style Login page

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
+import PersonIcon from "@material-ui/icons/Person";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
 
 import { loginAsync } from "./authSlice";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   container: {
-    width: "100%"
+    width: "80%",
+    margin: "0 auto",
   },
   form: {
-    margin: "0 auto",
-    padding: 3
-  }
-});
+    display: "grid",
+    gridTemplateRows: "1fr 1fr 1fr",
+    gridRowGap: "1em",
+  },
+  icon: {
+    display: "grid",
+    gridTemplateColumns: "0.3fr 10fr",
+    alignItems: "center",
+  },
+  logo: {
+    width: "1.2em",
+    height: "1.5em",
+  },
+  signInText: {
+    fontSize: "1.5em",
+  },
+  textField: {
+    width: "100%",
+  },
+  button: {
+    width: "10%",
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: "none",
+  },
+}));
 
 const Login = () => {
   const classes = useStyles();
@@ -24,16 +50,11 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
 
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  if (isAuthenticated) {
-    return <Redirect to="/" />;
-  }
-
   const loginUser = () => {
     dispatch(
       loginAsync({
         email,
-        password
+        password,
       })
     );
   };
@@ -41,28 +62,45 @@ const Login = () => {
   return (
     <div className={classes.container}>
       <div className={classes.form}>
-        <TextField
-          type="email"
-          label="Email"
-          onChange={e => setEmail(e.target.value)}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Password"
-          type="password"
-          onChange={e => setPassword(e.target.value)}
-          fullWidth
-          required
-        />
+        <div className={classes.icon}>
+          <PersonIcon className={classes.logo} />
+          <Typography component="p" className={classes.signInText}>
+            Sign Into Your Account
+          </Typography>
+        </div>
+        <div>
+          <TextField
+            type="email"
+            label="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            variant="outlined"
+            className={classes.textField}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            className={classes.textField}
+          />
+        </div>
         <Button
           disabled={!email || !password}
           variant="contained"
-          color="primary"
           onClick={loginUser}
+          color="primary"
+          className={classes.button}
         >
           Login
         </Button>
+        <Typography component="p">
+          Don't have an account?{" "}
+          <Link to="/account/register" className={classes.link}>
+            Signup
+          </Link>
+        </Typography>
       </div>
     </div>
   );
