53950ca5f56aee6173343a777763951f30c526b1
Sharan-ram <sharanram1993@gmail.com>
2020-04-15 14:18:52 +0530

    Use hitstoryPush to link to Profile page from UserProfiles

--- src/features/Post/Posts.js
+++ src/features/Post/Posts.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import moment from "moment";
 
 import Button from "@material-ui/core/Button";
