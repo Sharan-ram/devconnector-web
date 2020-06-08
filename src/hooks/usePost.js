a2d5f65523e05ec0e8c4c522108222bf0834f682
Sharan-ram <sharanram1993@gmail.com>
2020-06-08 12:16:35 +0530

    Fix like/unlike and add comment bugs

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import _ from "lodash";
 
 import { getPostByIdAsync } from "../features/Post/postSlice";
 
@@ -8,14 +9,8 @@ const usePost = (state, postId) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!state) {
-      dispatch(getPostByIdAsync(postId));
-    }
+    dispatch(getPostByIdAsync(postId));
   }, [dispatch, postId, state]);
-
-  if (state?.post) {
-    return [false, state.post, false];
-  }
 
   return [isLoading, post, error];
 };
